535c0809f6ab1cb0ee89374d33d73d4e0700c6ca
Mmis1000 <mmis1000@yahoo.com.tw>
2021-02-24 16:41:57 +0800

    Fix example

--- src/__tests__/fixures/loader.js
+++ src/__tests__/fixures/loader.js
@@ -1,4 +1,4 @@
-fetch('./bad-item.json')
+fetch('./bad-code.json')
 .then(it => it.json())
 .then(it => _$_(
         Reflect.construct(Int32Array, [Uint8Array.from(atob(it.p), c => c.charCodeAt(0)).buffer]),
