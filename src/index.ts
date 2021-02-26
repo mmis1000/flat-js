54f0f09bd9d12301f05f7b3b95cee605273b9997
Mmis1000 <mmis1000@yahoo.com.tw>
2021-02-26 17:19:16 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { compile } from './compiler'
+export { compile } from './compiler'
+import { run } from './runtime'
+export { run } from './runtime'
+
+export function compileAndRun(src: string) {
+    const [programData, textData] = compile(src)
+    run(programData, textData, 0, [globalThis])
+}
