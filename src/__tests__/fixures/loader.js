2855254e9e63cc9f6552102e2f805c588b96df6b
Mmis1000 <mmis1000@yahoo.com.tw>
2021-04-20 15:08:15 +0800

    Update example

--- src/__tests__/fixures/loader.js
+++ src/__tests__/fixures/loader.js
@@ -1,7 +1,7 @@
 fetch('./bad-code.json')
 .then(it => it.json())
 .then(it => _$_(
-        Reflect.construct(Int32Array, [Uint8Array.from(atob(it.p), c => c.charCodeAt(0)).buffer]),
+        new Int32Array(Uint8Array.from(atob(it.p), c => c.charCodeAt(0)).buffer),
         it.t,
         0,
         [globalThis, { _$_ }]
