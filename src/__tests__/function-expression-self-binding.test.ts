173e34a4e291936bac805718a4d3bfa66800ee3e
mmis1000 <mmis10002@gmail.com>
2026-04-27 16:46:30 +0800

    Fix named function expression self binding

---
+++ src/__tests__/function-expression-self-binding.test.ts
@@ -0,0 +1,30 @@
+import * as compiler from '../compiler'
+import * as runtime from '../runtime'
+
+test('named function expression self binding shadows outer minified variable names', () => {
+    const code = `
+(function(){
+    var t = false
+    var seen = 0
+    var self = {
+        add: function() {
+            return (function t(args) {
+                Array.prototype.forEach.call(args, function(arg) {
+                    if (arg && arg.length && typeof arg !== 'string') {
+                        t(arg)
+                    } else {
+                        seen++
+                    }
+                })
+            }(arguments), this)
+        }
+    }
+    self.add([1])
+    return seen
+})()
+`
+
+    const [program] = compiler.compile(code, { evalMode: true })
+
+    expect(runtime.run(program, 0, globalThis, [])).toBe(1)
+})
